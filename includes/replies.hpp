a010b263b64315f29a3cc9074a1db3fa17cdfe30
Chloe AUBRY <caubry@e1r7p12.clusters.42paris.fr>
2023-04-26 17:31:39 +0200

    cleaning PRIVMSG et NOTICE ok

--- includes/replies.hpp
+++ includes/replies.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   replies.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpoder <rpoder@student.42.fr>              +#+  +:+       +#+        */
+/*   By: caubry <caubry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:56:25 by rpoder            #+#    #+#             */
-/*   Updated: 2023/04/26 11:20:16 by rpoder           ###   ########.fr       */
+/*   Updated: 2023/04/26 17:12:36 by caubry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,15 @@ std::string RPL_ENDOFNAMES(ChannelMember &member, Channel &channel);
 std::string RPL_JOIN(ChannelMember &member, Channel &channel);
 
 std::string	RPL_CHANNELMODEIS(ChannelMember &member, Channel &channel,std::string reply_details);
+
 std::string RPL_KICK(User *user, Channel *channel, std::string nickname_kicked_out);
+
+std::string RPL_PRIVMSG_USER(User *user, User &receiver, std::string toSent);
+
+std::string RPL_PRIVMSG_CHANNEL(User *user, Channel &channel, std::string toSent);
+
+std::string RPL_NOTICE_CHANNEL(User *user, Channel &channel, std::string toSent);
+
+std::string RPL_NOTICE_USER(User *user, User &receiver, std::string toSent);
 
 #endif
